e3d480e5051f91288cd9db5cea3f01ce271471be
Michael Dauner <mdauner@gmail.com>
2019-10-12 20:59:28 -0500

    feat(action): support plugins
    BREAKING CHANGE: the action parameter is now an object { props, plugins }

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,41 @@
 <script>
   import tippy from './index';
+  import { followCursor } from 'tippy.js';
 
-  const tippyProps = {
-    content: 'Content',
+  const props = {
+    content: '<div class="tooltip">Styled tooltip text</div>',
+    followCursor: true,
+    onShow: function(instance) {
+      console.log(instance);
+    },
   };
+
+  const plugins = [followCursor];
 </script>
 
-<button use:tippy={tippyProps}>Save</button>
+<style>
+  .wrapper {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  span {
+    padding: 16px;
+    border-radius: 5px;
+    background-color: blue;
+    color: white;
+    cursor: pointer;
+  }
+
+  :global(.tooltip) {
+    color: orange;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+</style>
+
+<div class="wrapper">
+  <span use:tippy={{ props, plugins }}>Hover Me</span>
+</div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
-export default function(node, props) {
-  tippy(node, props);
+export default function(node, { props, plugins }) {
+  tippy(node, props, plugins);
 }
