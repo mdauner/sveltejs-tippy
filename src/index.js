8429d7eb48debded138677bb6142ab852be78710
AndrewLester <alester220@gmail.com>
2021-02-08 15:50:20 -0500

    Add update and destroy to tippy action

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import 'tippy.js/dist/tippy.css';
 
 export default function(node, props) {
   tippy(node, props);
+
+  return {
+    update(newProps) {
+        node._tippy.setProps(newProps);
+    },
+    destroy() {
+        node._tippy.destroy();
+    }
+  };
 }
